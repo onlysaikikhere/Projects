d2b60f91b18afd9983acd071152e27f49779377b
onlysaikihere <69111602+onlysaikikhere@users.noreply.github.com>
2024-07-09 21:10:35 +0530

    Create guess.py
    a python program to guess number

---
+++ guess.py
@@ -0,0 +1,15 @@
+import random
+def guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        guess = random.randint(1,x)
+        feedback=input(f"is {guess} too high (h) , too low (l) or correct (c)")
+        if feedback == 'h' :
+            high= guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+    print("yay you guessed the correct number!!")
+
+guess(100)
